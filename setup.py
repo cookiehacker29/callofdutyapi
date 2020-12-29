7ee156d76ac3883fc52f29edb410e9eae73d3763
cookiehacker <erwanaubry45@protonmail.com>
2020-12-29 19:54:51 +0100

    setup.py created

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name="callofdutyAPI",
+    version="0.0.1",
+    description="Connection to call of duty API",
+    author="cookiehacker",
+    packages=find_packages('.')
+)
+
+
+
+
